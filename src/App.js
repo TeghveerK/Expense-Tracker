1c65ebea8bbbf6ec2755f8a8bf567b08256a27c7
TeghveerK <tkooner@uci.edu>
2022-12-25 14:54:47 -0800

    Filtering and Lifting State Up

--- src/App.js
+++ src/App.js
@@ -25,9 +25,15 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  };
+
+  const addExpenseFilter = (expense) => {};
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
